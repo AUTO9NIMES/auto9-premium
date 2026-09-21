cd2df4b191dc5f4e865fc143ea025bf33fa75974
AUTO9NIMES <contact.nicolas.auto9@gmail.com>
2026-09-21 20:42:54 +0200

    fix: repair private subscription booking imports

--- app/reservation-abonnement/[token]/actions.ts
+++ app/reservation-abonnement/[token]/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import { supabaseRest } from "../../../lib/supabase";
+import { supabaseRest } from "../../lib/supabase";
 
 const UUID_REGEX =
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
